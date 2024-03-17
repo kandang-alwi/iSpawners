301311d7343bce0b2b3f3433bf8f33961827b2ff
Ridho <139139751+idhoorz@users.noreply.github.com>
2024-03-17 20:44:18 +0700

    Delete src/main/java/dev/jay/kacore directory

--- src/main/java/dev/jay/kacore/objects/Permission.java
+++
@@ -1,9 +0,0 @@
-package dev.jay.kacore.objects;
-
-public final class Permission {
-    public static final String STAFF = "kacore.staff";
-    public static final String ADMIN = "kacore.admin";
-    public static final String DONATOR = "karank.donate";
-    public static final String HIDDEN = "kacore.hidden";
-    public static final String GLOBAL_CHAT = "kacore.globalchat";
-}
